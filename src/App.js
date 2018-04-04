7db031c09cdf14e2603eec4664d415a3decf3dcc
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-04-03 23:41:19 -0400

    add Title component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Search from './Component/Search.js';
 import Buttonbox from './Component/Buttonbox.js';
+import Title from './Component/Title.js';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Search />
         <Buttonbox />
+        <Title />
       </div>
     );
   }
