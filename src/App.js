d0aed43e88c48a0104d381f266e656404dc56a1b
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-04-04 20:17:41 -0400

    Add router, rudimentary 404 page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import Search from './Component/Search.js';
 import ButtonContainer from './Component/ButtonContainer.js';
 import Title from './Component/Title.js';
 import PhotoContainer from './Component/PhotoContainer.js';
+import ErrNoMatch from './Component/ErrNoMatch.js';
+import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={MainWindow}/>
+          <Route component={ErrNoMatch}/>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+class MainWindow extends Component {
   render() {
     return (
       <div className="App">
---
+++ src/Component/ErrNoMatch.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ErrNoMatch extends Component {
+    render() {
+        return (
+        <p>No page or resource found at this URL. (404)</p>
+        );
+    }
+}
+
+export default ErrNoMatch;
+
