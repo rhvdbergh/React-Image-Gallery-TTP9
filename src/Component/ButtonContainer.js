80ee6beead7ff0ec59e584545724ad0a56bfc3f4
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-04-05 23:21:46 -0400

    styled buttons, title

--- src/Component/ButtonContainer.js
+++ src/Component/ButtonContainer.js
@@ -4,7 +4,7 @@ import Button from './Button';
 class ButtonContainer extends Component {
     render() {
         return (
-            <div>
+            <div className="ButtonContainer">
             <Button />
             <Button />
             <Button />    
